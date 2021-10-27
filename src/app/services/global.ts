98772cecdc62f3db5a59c4e576ccf4c8af800040
eduardo19Fu <ramieduar@gmail.com>
2021-10-26 18:47:51 -0600

    Listo para produccion despues de cambios en version produccion-1

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,5 @@
 export const global = {
-    url : 'http://localhost:8281/api'
+    // url : 'http://localhost:8281/api'
     // url: 'https://dimsa-app2.herokuapp.com/api'
-    // url: 'https://dimsa-new.herokuapp.com/api'
+    url: 'https://dimsa-new.herokuapp.com/api'
 };
